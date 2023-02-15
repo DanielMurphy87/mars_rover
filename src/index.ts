74fc69ce7d5a14d0e977bae50e941ba2506f4024
DanielMurphy87 <84238673+DanielMurphy87@users.noreply.github.com>
2023-02-15 22:38:51 +0200

    fix: added some input validation

--- src/index.ts
+++ src/index.ts
@@ -7,24 +7,50 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-// State variables
 let plateau: ReturnType<typeof createPlateau>;
 let rover: Rover;
 
 console.log('Welcome to the Mars Rover Program!\n');
 
 rl.question('Please enter the plateau\'s dimensions in the format "X Y": ', (dimensions: string) => {
-    const [width, height] = dimensions.split(' ').map(Number);
+    const pattern = /^(\d+)\s+(\d+)$/;
+    const match = dimensions.match(pattern);
+
+    if (!match) {
+        console.error('Invalid input. Please enter dimensions in the format "X Y".');
+        endProgram();
+        return;
+    }
+
+    const [width, height] = match.slice(1).map(Number);
     plateau = createPlateau(width, height);
     console.log(`\nPlateau created with dimensions ${width} x ${height}.\n`);
 
     rl.question('Please enter the rover\'s initial position and direction in the format "X Y D": ', (position: string) => {
-        const [x, y, direction] = position.toLocaleUpperCase().split(' ');
+        const pattern = /^(\d+)\s+(\d+)\s+([NSEWnsew])$/;
+        const match = position.toUpperCase().match(pattern);
+
+        if (!match) {
+            console.error('Invalid input. Please enter the initial position and direction in the format "X Y D".');
+            endProgram();
+            return;
+        }
+
+        const [x, y, direction] = match.slice(1);
         rover = createRover(Number(x), Number(y), direction as Rover['direction']);
         console.log(`\nRover created at position ${x}, ${y} facing ${direction}.\n`);
 
         rl.question('Please enter the rover\'s movements: ', (movement: string) => {
-            const actions = movement.toLocaleUpperCase().split('') as RoverInstruction[];
+            const pattern = /^[LMRAlmra]*$/;
+            const match = movement.toUpperCase().match(pattern);
+
+            if (!match) {
+                console.error('Invalid input. Please enter the rover\'s movements using only "L", "M", and "R" characters.');
+                endProgram();
+                return;
+            }
+
+            const actions = movement.split('') as RoverInstruction[];
             performActions(rover, plateau, actions);
             console.log(`\nRover finished at position ${rover.x} ${rover.y} facing ${rover.direction}.\n`);
 
