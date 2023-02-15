ce787e279f3ce25f697d92022081284b3b89fcd7
DanielMurphy87 <84238673+DanielMurphy87@users.noreply.github.com>
2023-02-15 21:52:02 +0200

    fix: remove unnecessary function from plateau

--- src/plateau.test.ts
+++ src/plateau.test.ts
@@ -1,4 +1,4 @@
-import { createPlateau, isWithinBounds, Plateau, Coordinate } from './plateau';
+import { createPlateau, Plateau, Coordinate } from './plateau';
 
 describe('createPlateau', () => {
     test('should return a plateau with the correct width and height', () => {
@@ -6,36 +6,5 @@ describe('createPlateau', () => {
         const height = 5;
         const plateau = createPlateau(width, height);
         expect(plateau).toEqual({ width, height });
-    });
-});
-
-describe('isWithinBounds', () => {
-    let plateau: Plateau;
-    beforeEach(() => {
-        plateau = createPlateau(10, 5);
-    });
-
-    test('should return true when coordinate is within bounds', () => {
-        const coordinate: Coordinate = { x: 2, y: 3 };
-        const result = isWithinBounds(plateau, coordinate);
-        expect(result).toBe(true);
-    });
-
-    test('should return false when x is out of bounds', () => {
-        const coordinate: Coordinate = { x: 12, y: 3 };
-        const result = isWithinBounds(plateau, coordinate);
-        expect(result).toBe(false);
-    });
-
-    test('should return false when y is out of bounds', () => {
-        const coordinate: Coordinate = { x: 2, y: -1 };
-        const result = isWithinBounds(plateau, coordinate);
-        expect(result).toBe(false);
-    });
-
-    test('should return false when x and y are out of bounds', () => {
-        const coordinate: Coordinate = { x: 12, y: -1 };
-        const result = isWithinBounds(plateau, coordinate);
-        expect(result).toBe(false);
     });
 });
--- src/plateau.ts
+++ src/plateau.ts
@@ -11,12 +11,3 @@ export interface Coordinate {
 export function createPlateau(width: number, height: number): Plateau {
     return { width, height };
 }
-
-export function isWithinBounds(plateau: Plateau, coordinate: Coordinate): boolean {
-    return (
-        coordinate.x >= 0 &&
-        coordinate.x <= plateau.width &&
-        coordinate.y >= 0 &&
-        coordinate.y <= plateau.height
-    );
-};
