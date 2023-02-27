2bd80a0a3de79679dda76294e44c55e8cbe48d20
DanielMurphy87 <84238673+DanielMurphy87@users.noreply.github.com>
2023-02-27 15:51:36 +0200

    fix: Updated index.ts so everything isn't all in one function

--- src/index.ts
+++ src/index.ts
@@ -10,61 +10,68 @@ const rl = readline.createInterface({
 let plateau: ReturnType<typeof createPlateau>;
 let rover: Rover;
 
-function startProgram() {
-    console.log('Welcome to the Mars Rover Program!\n');
-
-    rl.question('Please enter the plateau\'s dimensions in the format "X Y": ', (dimensions: string) => {
-        const pattern = /^(\d+)\s+(\d+)$/;
-        const match = dimensions.match(pattern);
-
-        if (!match) {
-            console.error('Invalid input. Please enter dimensions in the format "X Y".');
-            endProgram();
-            return;
-        }
-
-        const [width, height] = match.slice(1).map(Number);
-        plateau = createPlateau(width, height);
-        console.log(`\nPlateau created with dimensions ${width} x ${height}.\n`);
-
-        rl.question('Please enter the rover\'s initial position and direction in the format "X Y D": ', (position: string) => {
-            const pattern = /^(\d+)\s+(\d+)\s+([NSEW])$/;
-            const match = position.toUpperCase().match(pattern);
-
+async function getUserInput(question: string, pattern: RegExp): Promise<string> {
+    return new Promise((resolve) => {
+        rl.question(question, (input: string) => {
+            const match = input.match(pattern);
             if (!match) {
-                console.error('Invalid input. Please enter the initial position and direction in the format "X Y D".');
+                console.error(`Invalid input. Please enter input that matches the pattern "${pattern}".`);
                 endProgram();
                 return;
             }
-
-            const [x, y, direction] = match.slice(1);
-            rover = createRover(Number(x), Number(y), direction as Rover['direction']);
-            console.log(`\nRover created at position ${x}, ${y} facing ${direction}.\n`);
-
-            rl.question('Please enter the rover\'s movements: ', (movement: string) => {
-                const pattern = /^[LMRA]*$/;
-                const match = movement.toUpperCase().match(pattern);
-
-                if (!match) {
-                    console.error('Invalid input. Please enter the rover\'s movements using only "L", "M", and "R" characters.');
-                    endProgram();
-                    return;
-                }
-
-                const actions = movement.toUpperCase().split('') as RoverInstruction[];
-                performActions(rover, plateau, actions);
-                console.log(`\nRover finished at position ${rover.x} ${rover.y} facing ${rover.direction}.\n`);
-
-                rl.question('Would you like to restart the program? (Y/N)', (answer: string) => {
-                    if (answer.toUpperCase() === 'Y') {
-                        startProgram();
-                    } else {
-                        endProgram();
-                    }
-                });
-            });
+            resolve(input);
         });
     });
+}
+
+async function startProgram() {
+    console.log('Welcome to the Mars Rover Program!\n');
+
+    const dimensionsPattern = /^(\d+)\s+(\d+)$/;
+    const dimensions = await getUserInput(
+        'Please enter the plateau\'s dimensions in the format "X Y": ',
+        dimensionsPattern
+    );
+    const dimensionsMatch = dimensions.match(dimensionsPattern);
+    if (!dimensionsMatch) {
+        console.error(`Invalid input. Please enter input that matches the pattern "${dimensionsPattern}".`);
+        endProgram();
+        return;
+    }
+    const [width, height] = dimensionsMatch.slice(1).map(Number);
+    plateau = createPlateau(width, height);
+    console.log(`\nPlateau created with dimensions ${width} x ${height}.\n`);
+
+    const positionPattern = /^(\d+)\s+(\d+)\s+([NSEW])$/;
+    const position = await getUserInput(
+        'Please enter the rover\'s initial position and direction in the format "X Y D": ',
+        positionPattern
+    );
+    const positionMatch = position.match(positionPattern);
+    if (!positionMatch) {
+        console.error(`Invalid input. Please enter input that matches the pattern "${positionPattern}".`);
+        endProgram();
+        return;
+    }
+    const [x, y, direction] = positionMatch.slice(1);
+    rover = createRover(Number(x), Number(y), direction as Rover['direction']);
+    console.log(`\nRover created at position ${x}, ${y} facing ${direction}.\n`);
+
+    const movementPattern = /^[LMRA]*$/;
+    const movement = await getUserInput(
+        'Please enter the rover\'s movements: ',
+        movementPattern
+    );
+    const actions = movement.toUpperCase().split('') as RoverInstruction[];
+    performActions(rover, plateau, actions);
+    console.log(`\nRover finished at position ${rover.x} ${rover.y} facing ${rover.direction}.\n`);
+
+    const restart = await getUserInput('Would you like to restart the program? (Y/N)', /^[YN]$/i);
+    if (restart.toUpperCase() === 'Y') {
+        startProgram();
+    } else {
+        endProgram();
+    }
 }
 
 function endProgram() {
